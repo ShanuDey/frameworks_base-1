1b17f1dcdbd6e5f6e5abab5cf62f039a6cd9e575
zfu <zfu@codeaurora.org>
2018-03-01 13:46:12 +0800

    add perf_service into system_process.
    Performance boost requests to Perf HAL from untrusted
    apps are no more accessible through hw binder.
    
    Now, boost requests from untrusted apps can be routed
    via Perf service.
    
    CRs-Fixed: 2198019
    Change-Id: I524d38a61608069028b3496a189118d833575f4b

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -567,6 +567,7 @@ public final class ViewRootImpl implements ViewParent,
         mScreenHeight = Math.max(dm.widthPixels, dm.heightPixels);
         mScreenWidth = Math.min(dm.widthPixels, dm.heightPixels);
 
+        mPerf = new BoostFramework(context);
     }
 
     public static void addFirstDrawHandler(Runnable callback) {
@@ -3215,9 +3216,6 @@ public final class ViewRootImpl implements ViewParent,
         if (mAttachInfo.mViewScrollChanged) {
             if (mHaveMoveEvent && !mIsPerfLockAcquired) {
                 mIsPerfLockAcquired = true;
-                if (mPerf == null) {
-                    mPerf = new BoostFramework();
-                }
                 if (mPerf != null) {
                     String currentPackage = mContext.getPackageName();
                     mPerf.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST, currentPackage, -1, BoostFramework.Scroll.PREFILING);
