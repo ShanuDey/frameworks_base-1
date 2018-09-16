12227d44721deb3311cf25f7b9c855e7b1699eb5
bigrushdog <randall.rushing@gmail.com>
2018-09-15 17:20:30 -0700

    Initial checkin on Pie for SmartNav navigation suite [1/4]
    Interesting changes for bring up:
    
    The DUI project is now called SmartNav in a effort to bring
    broader generalization to the project
    
    The SmartNav settings have been moved to a separate project.
    This will allow for MUCH EASIER adaptation for projects in which
    only minimal touching of Settings and project specific settings
    is required.
    
    Elimination of the previously required utils jar shared library!
    We moved all the utils classes to fwb. This eliminated the need
    to hax up bp files and touch makefiles all over to use utils
    methods, as well as simplifies project integration.
    
    Wrapping stock navigation implementation in the Navigator
    abstraction is becoming increasingly difficult as more
    "things" interact with stock navigation buttons. We handle this
    situation with button null checks as well as "isStockNav()" checks.
    
    As SystemUI continues to compartmentalize, required notification media
    methods in StatusBar moved to a separate class. Adaptation for Pulse
    required the introduction of a new callback from NotificationMediaManager
    
    @xyyx: add keys to backup and some fixes for our hw keys implementation

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -89,6 +89,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.AudioAttributes;
 import android.media.MediaMetadata;
+import android.media.session.MediaController;
 import android.metrics.LogMaker;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -150,6 +151,7 @@ import com.android.internal.util.hwkeys.ActionUtils;
 import com.android.internal.util.hwkeys.PackageMonitor;
 import com.android.internal.util.hwkeys.PackageMonitor.PackageChangedListener;
 import com.android.internal.util.hwkeys.PackageMonitor.PackageState;
+import com.android.internal.utils.SmartPackageMonitor;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.MessagingGroup;
 import com.android.internal.widget.MessagingMessage;
@@ -182,6 +184,7 @@ import com.android.systemui.fragments.FragmentHostManager;
 import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.keyguard.ScreenLifecycle;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
+import com.android.systemui.navigation.Navigator;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.qs.QS;
 import com.android.systemui.plugins.statusbar.NotificationSwipeActionHelper.SnoozeOption;
@@ -735,6 +738,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
 
         mDisplay = mWindowManager.getDefaultDisplay();
+
         updateDisplaySize();
 
         mPackageMonitor = new PackageMonitor();
@@ -885,6 +889,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         Dependency.get(ConfigurationController.class).addCallback(this);
 
+        mFlashlightController = Dependency.get(FlashlightController.class);
     }
 
     // ================================================================================
@@ -1287,6 +1292,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             if (mLightBarController != null) {
                 mNavigationBar.setLightBarController(mLightBarController);
             }
+            if (!mNavigationBar.isUsingStockNav()) {
+                ((NavigationBarFrame)mNavigationBarView).disableDeadZone();
+            }
             mNavigationBar.setCurrentSysuiVisibility(mSystemUiVisibility);
         });
     }
@@ -1301,6 +1309,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             fragmentHost.getFragmentManager().beginTransaction().remove(mNavigationBar).commit();
             mNavigationBar = null;
         }
+    }
+
+    public NotificationMediaManager getMediaManager() {
+        return mMediaManager;
     }
 
     /**
@@ -2315,6 +2327,9 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         if (!isExpanded) {
             mRemoteInputManager.removeRemoteInputEntriesKeptUntilCollapsed();
+        }
+        if (mNavigationBar != null) {
+            mNavigationBar.setPanelExpanded(isExpanded);
         }
     }
 
@@ -3778,7 +3793,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                                 ActionConstants
                                         .getDefaults(ActionConstants.HWKEYS));
                     }
-                }
+                    ActionUtils
+                            .resolveAndUpdateButtonActions(ctx, ActionConstants
+                                    .getDefaults(ActionConstants.SMARTBAR));
+                    ActionUtils.resolveAndUpdateButtonActions(ctx,
+                            ActionConstants.getDefaults(ActionConstants.FLING));
+		}
             });
             thread.setPriority(Process.THREAD_PRIORITY_BACKGROUND);
             thread.start();
@@ -4795,9 +4815,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         return getMaxNotificationsWhileLocked(false /* recompute */);
     }
 
-    // TODO: Figure out way to remove these.
-    public NavigationBarView getNavigationBarView() {
-        return (mNavigationBar != null ? (NavigationBarView) mNavigationBar.getView() : null);
+    // TODO: Figure out way to remove this.
+    public Navigator getNavigationBarView() {
+        return mNavigationBar != null ? mNavigationBar.getNavigator() : null;
     }
 
     public View getNavigationBarWindow() {
