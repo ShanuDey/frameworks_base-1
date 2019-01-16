38b2ae922129e512829c0e076f2eff8574a2d9a1
mdeejay <mdjrussia@gmail.com>
2019-01-16 05:25:29 +0100

    benzo -> beast

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenrecordTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenrecordTile.java
@@ -109,7 +109,7 @@ public class ScreenrecordTile extends QSTileImpl<BooleanState> {
                 return;
             }
             ComponentName cn = new ComponentName("com.android.systemui",
-                    "com.android.systemui.benzo.screenrecord.TakeScreenrecordService");
+                    "com.android.systemui.beast.screenrecord.TakeScreenrecordService");
             Intent intent = new Intent();
             intent.setComponent(cn);
             ServiceConnection conn = new ServiceConnection() {
