78edc67b3d480009dbffc1c7c68ee87746645335
mdeejay <mdjrussia@gmail.com>
2019-02-05 14:07:02 +0100

    Revert: "Add ScreenStabilizationTile in QS"

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -46,7 +46,6 @@ import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.SoundTile;
-import com.android.systemui.qs.tiles.ScreenStabilizationTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -115,8 +114,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new SoundTile(mHost);
             case "screenrecord":
                 return new ScreenrecordTile(mHost);
-			case "screenstabilization":
-				return new ScreenStabilizationTile(mHost);
             case "hwkeys":
                 return new HWKeysTile(mHost);
             case "gaming":
