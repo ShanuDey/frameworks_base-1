e8ee03c88b6812248c7949cab7755d230f0f4a46
mdeejay <mdjrussia@gmail.com>
2019-02-05 14:10:15 +0100

    Revert: "Screen Stabiization [1/3]"

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -74,7 +74,6 @@ import com.android.server.coverage.CoverageService;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.display.ColorDisplayService;
 import com.android.server.display.DisplayManagerService;
-import com.android.server.display.ScreenStabilization;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.emergency.EmergencyAffordanceService;
 import com.android.server.fingerprint.FingerprintService;
@@ -1587,11 +1586,7 @@ public final class SystemServer {
 
             traceBeginAndSlog("StartCrossProfileAppsService");
             mSystemServiceManager.startService(CrossProfileAppsService.class);
-            traceEnd();
-
-            traceBeginAndSlog("Starting ScreenStabilization Service");
-            mSystemServiceManager.startService(ScreenStabilization.class);
-            traceEnd();	    
+            traceEnd();    
 
             try {
                 Slog.i(TAG, "EdgeGesture service");
