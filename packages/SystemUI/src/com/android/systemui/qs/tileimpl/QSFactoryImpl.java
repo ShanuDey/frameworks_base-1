c3f15b8dc3e3b5574134eac19633d75bbe763cc6
Joey <joey@lineageos.org>
2019-05-11 13:27:56 +0200

    SystemUI: Add reading mode tile
    Author: Joey <joey@lineageos.org>
    Date:   Wed May 16 17:16:06 2018 +0200
    
        base: add reading mode tile
    
        Change-Id: I1c515cbeff0dd5a1fa0c00bc9390a66eb104c7f0
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sat May 26 00:58:57 2018 +0300
    
        SystemUI: Fix reading mode tile accessibility strings
    
        Change-Id: I4f0af56e4947b037e1e2dda79132523d13458818
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Wed Aug 8 18:59:25 2018 +0200
    
        SystemUI: Update reading mode tile for new API
    
        Change-Id: I9fbf1b4de30079c987b75e37e52e234fa2f86c5d
    SystemUI: Add reading mode tile
    
    Author: Joey <joey@lineageos.org>
    Date:   Wed May 16 17:16:06 2018 +0200
    
        base: add reading mode tile
    
        Change-Id: I1c515cbeff0dd5a1fa0c00bc9390a66eb104c7f0
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sat May 26 00:58:57 2018 +0300
    
        SystemUI: Fix reading mode tile accessibility strings
    
        Change-Id: I4f0af56e4947b037e1e2dda79132523d13458818
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Wed Aug 8 18:59:25 2018 +0200
    
        SystemUI: Update reading mode tile for new API
    
        Change-Id: I9fbf1b4de30079c987b75e37e52e234fa2f86c5d

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -45,6 +45,7 @@ import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.OnTheGoTile;
+import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
@@ -134,6 +135,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SoundSearchTile(mHost);
             case "livedisplay":
                 return new LiveDisplayTile(mHost);
+            case "reading_mode":
+                return new ReadingModeTile(mHost);
         }
 
         // Intent tiles.
